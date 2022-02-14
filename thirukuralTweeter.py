1a51f819dab39206613d152d4fe829243966859e
Ashokkumar <iashok22@gmail.com>
2022-02-14 12:22:05 +0530

    Update thirukuralTweeter.py

--- thirukuralTweeter.py
+++ thirukuralTweeter.py
@@ -4,38 +4,40 @@ import random
 
 S = requests.Session()
 
-URL = "https://api-thirukkural.vercel.app/api?num="
+def tweetRandomly():
+    URL = "https://api-thirukkural.vercel.app/api?num="
+    idNum = random.randint(1, 1330)
+    print(idNum)
+    URL = URL + str(idNum)
+    R = S.get(url=URL)
+    DATA = R.json()
 
-idNum = random.randint(1, 1330)
-print(idNum)
-URL = URL + str(idNum)
-R = S.get(url=URL)
-DATA = R.json()
+    # print(DATA["parse"]["text"]["*"])
+    section = DATA["sect_eng"]
+    chaptor_grp = DATA["chapgrp_eng"]
+    #chaptor = DATA["chap_eng"]
+    kural = DATA["eng"]
+    kuralExp = DATA["eng_exp"]
+    static_text = "Thirukural - திருக்குறள்"
+    breadcrum = section + "/" + chaptor_grp
+    tweet_msg = static_text + "\n" + breadcrum + "\n\n" + kural + "\n\n" + kuralExp
+    print(tweet_msg)
+    print(len(tweet_msg))
+    
+    # Authenticate to Twitter
+    auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
+    auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
 
-#print(DATA["parse"]["text"]["*"])
-section = DATA["sect_eng"]
-chaptor_grp = DATA["chapgrp_eng"]
-#chaptor = DATA["chap_eng"]
-kural = DATA["eng"]
-kuralExp = DATA["eng_exp"]
-static_text = "Thirukural - திருக்குறள்"
-breadcrum = section +"/"+ chaptor_grp
-tweet_msg = static_text +"\n"+ breadcrum +"\n\n"+ kural +"\n\n"+ kuralExp
-print(tweet_msg)
-print(len(tweet_msg))
-# Authenticate to Twitter
-auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
-auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
+    # Create API object
+    api = tweepy.API(auth)
+    
+    # Create a tweet
+    api.update_status(tweet_msg)
+    print("tweet published")
 
-# Create API object
-api = tweepy.API(auth)
-api.verify_credentials()
+
 try:
-    api.verify_credentials()
-    print("Authentication Successful")
+    tweetRandomly()
 except:
-    print("Authentication Error")
-
-# Create a tweet
-api.update_status(tweet_msg)
-print("tweet published")
+    #Calling the same function again on exception for fail-safe. this function will fail if the content has more than 280 chars, so retrying.
+    tweetRandomly()
